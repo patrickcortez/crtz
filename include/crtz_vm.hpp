9b11c5d20faf95a28b260bbe563fb61ca3881558
patrickcortez <patrickcortez2024@gmail.com>
2025-08-26 11:18:44 +0800

    ver4

--- include/crtz_vm.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-namespace CRTZ {
-// Load a .crtzc file and execute it using your existing runtime systems.
-// You can implement this by extending your interpreter to read bytecode
-// instead of parsing source at runtime.
-bool runProgramBytecode(const std::string& path);
-}
